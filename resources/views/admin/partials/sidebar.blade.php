0fd0c9319a24edcff589e502afa8ca9045d3261d
Johnny3d7 <sregis.ble@gmail.com>
2021-12-10 09:32:31 +0000

    Démarrage app

---
+++ resources/views/admin/partials/sidebar.blade.php
@@ -0,0 +1,179 @@
+<nav class="sidebar">
+    <div class="logo d-flex justify-content-between">
+        <a class="large_logo" href="index-2.html"><img src="{{ url('img/logo1.png') }}" style="" alt=""></a>
+        <a class="small_logo" href="index-2.html"><img src="{{ url('img/mini_logo.png') }}" alt=""></a>
+        <div class="sidebar_close_icon d-lg-none">
+            <i class="ti-close"></i>
+        </div>
+    </div>
+    <ul id="sidebar_menu">
+        <li class="">
+          <a  href="{{ route('admin.index') }}" aria-expanded="false">
+            <div class="nav_icon_small">
+              <img src="{{ url('img/menu-icon/7.svg') }}" alt="">
+          </div>
+          <div class="nav_title">
+              <span>Tableau de bord</span>
+          </div>
+          </a>
+        </li>
+        <li class="">
+            <a class="has-arrow" href="#" aria-expanded="false">
+                <div class="nav_icon_small">
+                    <img src="{{ url('img/menu-icon/dashboard.svg') }}" alt="">
+                </div>
+                <div class="nav_title">
+                    <span>Contrats </span>
+                </div>
+            </a>
+            <ul>
+                <li><a href="index_2.html">Ajouter un contrat</a></li>
+                <li><a href="index_2.html">En cours</a></li>
+                <li><a href="index_3.html">Expirés</a></li>
+  
+            </ul>
+        </li>
+        <li class="">
+            <a class="has-arrow" href="#" aria-expanded="false">
+                <div class="nav_icon_small">
+                    <img src="{{ url('img/menu-icon/2.svg') }}" alt="">
+                </div>
+                <div class="nav_title">
+                    <span>Adhérents </span>
+                </div>
+            </a>
+            <ul>
+              <li><a href="editor.html">Tous les adhérents</a></li>
+              <li><a href="mail_box.html">Tous les Souscripteurs</a></li>
+            </ul>
+        </li>
+        <li class="">
+            <a   class="has-arrow" href="#" aria-expanded="false">
+              <div class="nav_icon_small">
+                <img src="{{ url('img/menu-icon/3.svg') }}" alt="">
+            </div>
+            <div class="nav_title">
+                <span>Cas assistés</span>
+            </div>
+            </a>
+            <ul>
+              <li><a href="login.html">Liste des cas</a></li>
+              <li><a href="resister.html">Créer un cas</a></li>
+            </ul>
+        </li>
+        <li class="">
+            <a   class="has-arrow" href="#" aria-expanded="false">
+              <div class="nav_icon_small">
+                <img src="{{ url('img/menu-icon/4.svg') }}" alt="">
+            </div>
+            <div class="nav_title">
+                <span>Cotisations</span>
+            </div>
+            </a>
+            <ul>
+              <li><a href="admin_list.html">Annuelles</a></li>
+              <li><a href="add_new_admin.html">Exceptionnelles</a></li>
+            </ul>
+        </li>
+        <li class="">
+            <a   class="has-arrow" href="#" aria-expanded="false">
+              <div class="nav_icon_small">
+                <img src="{{ url('img/menu-icon/4.svg') }}" alt="">
+            </div>
+            <div class="nav_title">
+                <span>Dépenses</span>
+            </div>
+            </a>
+            <ul>
+              <li><a href="admin_list.html">Ajouter une dépense</a></li>
+              <li><a href="add_new_admin.html">Liste des dépenses</a></li>
+            </ul>
+        </li>
+        <li class="">
+            <a   class="has-arrow" href="#" aria-expanded="false">
+              <div class="nav_icon_small">
+                <img src="{{ url('img/menu-icon/11.svg') }}" alt="">
+            </div>
+            <div class="nav_title">
+                <span>Campagnes</span>
+            </div>
+            </a>
+            <ul>
+              <li><a href="module_setting.html">Email</a></li>
+              <li><a href="role_permissions.html">Sms</a></li>
+            </ul>
+        </li>
+        <li class="">
+            <a   class="has-arrow" href="#" aria-expanded="false">
+              <div class="nav_icon_small">
+                <img src="{{ url('img/menu-icon/5.svg') }}" alt="">
+            </div>
+            <div class="nav_title">
+                <span>Demandes d'adhésion</span>
+            </div>
+            </a>
+            <ul>
+              <li><a href="user_list.html">A traiter</a></li>
+              <li><a href="add_new_user.html">Valider</a></li>
+              <li><a href="add_new_user.html">Rejeter</a></li>
+            </ul>
+        </li>
+        <li class="">
+            <a  class="has-arrow" href="#" aria-expanded="false">
+              <div class="nav_icon_small">
+                  <img src="{{ url('img/menu-icon/8.svg') }}" alt="">
+              </div>
+              <div class="nav_title">
+                  <span>Configurations</span>
+              </div>
+            </a>
+            <ul>
+              <li><a href="Basic_Elements.html">Droit d'inscription</a></li>
+              <li><a href="Groups.html">Cotisation annuelle</a></li>
+              <li><a href="Max_Length.html">Cotisation exceptionnelle</a></li>
+              <li><a href="Max_Length.html">Traitement et kits d'inscription</a></li>
+            </ul>
+          </li>
+        <li class="">
+            <a  class="has-arrow" href="#" aria-expanded="false">
+              <div class="nav_icon_small">
+                  <img src="{{ url('img/menu-icon/11.svg') }}" alt="">
+              </div>
+              <div class="nav_title">
+                  <span>Administrateurs</span>
+              </div>
+            </a>
+            <ul>
+              <li><a href="dark_sidebar.html">Liste des administrateurs</a></li>
+              <li><a href="light_sidebar.html">Créer un administrateur</a></li>
+            </ul>
+        </li>
+        <li class="">
+            <a  class="has-arrow" href="#" aria-expanded="false">
+              <div class="nav_icon_small">
+                  <img src="{{ url('img/menu-icon/12.svg') }}" alt="">
+              </div>
+              <div class="nav_title">
+                  <span>Mon compte</span>
+              </div>
+            </a>
+            <ul>
+              <li><a href="Minimized_Aside.html">Mes informations</a></li>
+              <li><a href="empty_page.html">Modifier mon mot de passe</a></li>
+            </ul>
+        </li>
+        <li class="">
+          <a href="Board.html" aria-expanded="false">
+              <div class="nav_icon_small">
+                  <img src="{{ url('img/menu-icon/9.svg') }}" alt="">
+              </div>
+              <div class="nav_title">
+                  <span>Se déconnecter</span>
+              </div>
+          </a>
+        </li>
+        
+
+
+      </ul>
+</nav>
---
+++ routes/web.php
@@ -0,0 +1,76 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::prefix('/admin')->namespace('App\Http\Controllers\Admin')->name('admin.')->group( function(){
+    Route::get('/index', 'HomeController@index')->name('index');
+
+
+    //Adhérents routes
+    Route::get('/adherents', 'AdherentController@index')->name('adherent.index');
+    Route::get('/souscripteurs', 'AdherentController@souscripteurs')->name('souscripteur.index');
+
+    //Contrats 
+    Route::get('/contrats', 'ContratController@index')->name('contrat.index');
+    Route::get('/contrats/expire', 'ContratController@expire')->name('contrat.expire');
+    Route::get('/contrat/create', 'ContratController@create')->name('contrat.create');
+    Route::post('/contrat/store', 'ContratController@store')->name('contrat.store');
+
+    //Dépenses
+    Route::get('/depenses', 'DepenseController@index')->name('depense.index');
+    Route::get('/depense/create', 'DepenseController@create')->name('depense.create');
+    Route::post('/depense/store', 'DepenseController@store')->name('depense.store');
+
+    //Cas assisté
+    Route::get('/assistances', 'AssistanceController@index')->name('assistance.index');
+    Route::get('/assistance/create', 'AssistanceController@create')->name('assistance.create');
+    Route::post('/assistance/store', 'AssistanceController@store')->name('assistance.store');
+
+    //Demande adhésion en ligne
+    Route::get('/demandes-a-traiter', 'DemandeController@index')->name('demande.index');
+    Route::get('/demandes/valider', 'DemandeController@valider')->name('demande.valider');
+    Route::get('/demandes/refuser', 'DemandeController@refuser')->name('demande.refuser');
+
+
+    //Configuration
+    
+    //Droit d'inscription
+    Route::get('/prix-droit-inscription', 'ConfigurationController@droitInscription')->name('droit-inscription.index');
+    Route::post('/prix-droit-inscription/store', 'ConfigurationController@droitInscriptionStore')->name('droit-inscription.store');
+
+    //Cotisations annuelles
+    Route::get('/prix-cotisation-annuelle', 'ConfigurationController@prixCotisationAnnuelle')->name('prix-cotisation-annuelle.index');
+    Route::post('/prix-cotisation-annuelle/store', 'ConfigurationController@prixCotisationAnnuelleStore')->name('prix-cotisation-annuelle.store');
+
+    //Cotisations exceptionnelles
+    Route::get('/prix-cotisation-exceptionnelle', 'ConfigurationController@prixCotisationExceptionnelle')->name('prix-cotisation-exceptionnelle.index');
+    Route::post('/prix-cotisation-exceptionnelle/store', 'ConfigurationController@prixCotisationExceptionnelleStore')->name('prix-cotisation-exceptionnelle.store');
+
+    //Kit d'inscription
+    Route::get('/prix-kit', 'ConfigurationController@prixKit')->name('prix-kit.index');
+    Route::post('/prix-kit/store', 'ConfigurationController@prixKitStore')->name('prix-kit.store');
+
+});
+
+// Formulaire demande d'adhésion en ligne
+Route::get('/demande/create', 'App\Http\Controllers\Admin\DemandeController@create')->name('demande.create');
+Route::post('/demande/store', 'App\Http\Controllers\Admin\DemandeController@store')->name('demande.store');
+
+
+
+
